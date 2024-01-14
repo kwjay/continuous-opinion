6aad91d7353ab18735f491e8b3321095d47ccfd5
kwjay <kwjay1002@gmail.com>
2024-01-14 16:26:16 +0100

    Better layout and functionality

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,6 @@
 import java .awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class MainFrame {
@@ -6,18 +8,51 @@ public class MainFrame {
     static float convergence = 0.3f;
     public static void main(String[] s) {
         JFrame frame = new JFrame("Continuous Opinion Simulation");
+        frame.setLayout(new GridBagLayout());
+        GridBagConstraints frameConstraints = new GridBagConstraints();
         SimFrame simFrame = new SimFrame(threshold, convergence);
-        frame.add(simFrame);
-        frame.setLayout(new FlowLayout());
-        frame.setSize(400, 400);
-        frame.setMinimumSize(new Dimension(400, 400));
+        Thread simulationThread = new Thread(simFrame);
+        frameConstraints.fill = GridBagConstraints.BOTH;
+        frameConstraints.weightx = 1;
+        frameConstraints.weighty = 1;
+        frameConstraints.gridheight = 3;
+        frameConstraints.gridwidth = 3;
+        frame.add(simFrame, frameConstraints);
+        JButton startButton = new JButton("START");
+        startButton.addActionListener(e -> simulationThread.start());
+        startButton.setFont(new Font("Arial", Font.BOLD, 25));
+        frameConstraints.fill = GridBagConstraints.BOTH;
+        frameConstraints.gridx = 0;
+        frameConstraints.gridy = 3;
+        frameConstraints.gridheight = 1;
+        frameConstraints.gridwidth = 1;
+        frame.add(startButton, frameConstraints);
+        JButton resetButton = new JButton("RESET");
+        resetButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        resetButton.setFont(new Font("Arial", Font.BOLD, 25));
+        frameConstraints.fill = GridBagConstraints.BOTH;
+        frameConstraints.gridx = 1;
+        frameConstraints.gridy = 3;
+        frameConstraints.gridheight = 1;
+        frameConstraints.gridwidth = 1;
+        frame.add(resetButton, frameConstraints);
+        JSlider thresholdSlider = new JSlider();
+        frameConstraints.fill = GridBagConstraints.NONE;
+        frameConstraints.gridx = 0;
+        frameConstraints.gridy = 4;
+        frameConstraints.gridheight = 4;
+        frameConstraints.gridwidth = 4;
+        frame.add(thresholdSlider, frameConstraints);
+        frame.setSize(550, 550);
+        frame.setMinimumSize(new Dimension(550, 550));
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        while (true) {
-            simFrame.runSim();
-        }
-
     }
 }
 
--- src/SimFrame.java
+++ src/SimFrame.java
@@ -1,13 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.Random;
 
-public class SimFrame extends JPanel {
-    private final int lattice_size = 400;
+public class SimFrame extends JPanel implements Runnable{
+    private final int lattice_size = 500;
     Agent[][] lattice = new Agent [lattice_size][lattice_size];
 
     public SimFrame(float threshold, float convergence) {
-        setBorder(BorderFactory.createLineBorder(Color.gray, 4));
+        setBorder(BorderFactory.createLineBorder(Color.gray, 1));
         for (int x = 0; x < this.lattice_size; x++) {
             for (int y = 0; y < this.lattice_size; y++) {
                 float opinion = (float)Math.random();
@@ -20,17 +19,19 @@ public class SimFrame extends JPanel {
         return new Dimension(300, 300);
     }
 
-    public void runSim() {
-        int agentX = (int)(Math.random() * lattice_size - 1);
-        int agentY = (int)(Math.random() * lattice_size - 1);
-        int[] neighbourXY = findNeighbour(agentX, agentY);
-        int neighbourX = neighbourXY[0];
-        int neighbourY = neighbourXY[1];
-        float agentOpinion = lattice[agentX][agentY].getOpinion();
-        float neighbourOpinion = lattice[neighbourX][neighbourY].getOpinion();
-        lattice[agentX][agentY].agentMet(neighbourOpinion);
-        lattice[neighbourX][neighbourY].agentMet(agentOpinion);
-        repaint();
+    public void run() {
+        while (true) {
+            int agentX = (int) (Math.random() * lattice_size - 1);
+            int agentY = (int) (Math.random() * lattice_size - 1);
+            int[] neighbourXY = findNeighbour(agentX, agentY);
+            int neighbourX = neighbourXY[0];
+            int neighbourY = neighbourXY[1];
+            float agentOpinion = lattice[agentX][agentY].getOpinion();
+            float neighbourOpinion = lattice[neighbourX][neighbourY].getOpinion();
+            lattice[agentX][agentY].agentMet(neighbourOpinion);
+            lattice[neighbourX][neighbourY].agentMet(agentOpinion);
+            repaint();
+        }
     }
 
     private int[] findNeighbour(int agentX, int agentY) {
@@ -63,7 +64,7 @@ public class SimFrame extends JPanel {
         for (int x = 0; x < lattice_size - 1; x++) {
             for (int y = 0; y < lattice_size - 1; y++) {
                 frameGraphics.setColor(calcColor(lattice[x][y]));
-                int square_size = 10;
+                int square_size = 5;
                 frameGraphics.fillRect(x * square_size, y * square_size,
                         square_size, square_size);
             }
